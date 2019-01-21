12ab3c8871480c9f294b0dba06d1f7e24e0f9448
zhaoruntong.falcon <zhaoruntong.falcon@shandagames.com>
2019-01-22 00:49:23 +0800

    wechat server demo

---
+++ Serv/wechatserv.py
@@ -0,0 +1,5 @@
+#coding=utf8
+import itchat
+
+itchat.auto_login(hotReload=True)
+itchat.send(u'看到我的python脚本了吗', 'filehelper')
--- base.py
+++ base.py
@@ -7,6 +7,7 @@ def main():
 	os.system('pip3 install pymssql')
 	os.system('pip3 install pymysql')
 	os.system('pip3 install configparser')
+	os.system('pip3 install itchat')
 
 if __name__ == '__main__':
 	main()
