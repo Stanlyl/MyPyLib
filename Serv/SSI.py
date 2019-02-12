71f694e96d86854575efd2ce194483fe8d056afc
赵润彤.Falcon <zhaoruntong.falcon@shandagames.com>
2019-02-12 13:20:20 +0800

    func SSI

--- Serv/SSI.py
+++ Serv/SSI.py
@@ -1,12 +1,31 @@
 from ctypes import *
-dll = cdll.LoadLibrary('SSICoder.dll')
-dll.hello.restype = c_char_p
-#print(dll.hello().decode())
-dll.SSI_Encrypt.argtypes=[POINTER(c_char)]
-dll.SSI_Encrypt.restype = c_char_p
-STR = (c_char * 128)(*bytes('Hello Python','utf-8'))
-cast(STR, POINTER(c_char))
-code = dll.SSI_Encrypt(STR)
-print(code)
-wer = dll.SSI_Encrypt(code)
-print(wer.decode())
+import os
+
+def SSI_init():
+	global dll
+	dll = cdll.LoadLibrary('SSICoder.dll')
+	dll.Greetings.restype = c_char_p
+	dll.SSI_Encrypt.restype = c_char_p
+	dll.SSI_Encrypt.argtypes=[POINTER(c_char)]
+
+#print(dll.Greetings().decode())
+
+def OUT_process(message):
+	STR = (c_char * 128)(*bytes(message,'utf-8'))
+	cast(STR, POINTER(c_char))
+	return dll.SSI_Encrypt(STR)
+
+def In_process(code):
+	return dll.SSI_Encrypt(code).decode()
+
+def main():
+	SSI_init()
+	code = OUT_process('programme never die')
+	print(code)
+	message = In_process(code)
+	print(message)
+
+if __name__ == '__main__':
+	main()
+	print(os.path.abspath('../'))
+	
