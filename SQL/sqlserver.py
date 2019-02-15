73696efc9179ed459ce01a9bec85a159f67936ea
zhaoruntong.falcon <zhaoruntong.falcon@shandagames.com>
2019-02-15 10:12:30 +0800

    sql ready to update

--- SQL/sqlserver.py
+++ SQL/sqlserver.py
@@ -104,7 +104,7 @@ class SQLServer:
 
 
 if __name__ == '__main__':
-    msg = SQLServer(server="localhost",user="sa",password="password",database="database")
+    msg = SQLServer(server="192.168.0.105",user="ASN",password="741499686yqy",database="ADBS")
     msg.SelectQuery(msg.SQLSelect(Distinct=0,Listname=["*"],Formname="SNDA_Finance_List",const="FinaClass1Code = '10'"))
     #print(msg.SQLUpdate(Update={'CostType': 1.4,'BuyClass':'test','coflm':'QA'},Formname="SNDA_Finance_List",const="FinaC.4lass1Code = '10'"))
     #msg.BuckupQuery("D:\\SQLbackUP")
